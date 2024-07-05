ca1074e59d3143b48fd19859cc1d43617f6e5f1a
Abu <104144116+a6uxa4@users.noreply.github.com>
2024-07-05 12:36:31 +0600

    fixe

--- src/screens/ChartProfit/index.tsx
+++ src/screens/ChartProfit/index.tsx
@@ -16,6 +16,7 @@ import {
   VictoryAxis,
   VictoryTheme,
   VictoryGroup,
+  VictoryLine,
 } from 'victory-native';
 import {G} from 'react-native-svg';
 
@@ -156,7 +157,8 @@ export const ChartProfit = () => {
         <VictoryChart
           theme={VictoryTheme.material}
           width={hourlyData.length * 50}
-          height={300}>
+          padding={{top: 20, bottom: 50, left: 50, right: 50}}
+          height={180}>
           <VictoryGroup offset={10}>
             <VictoryBar
               style={{data: {fill: '#048FF3', width: 8}}}
@@ -218,6 +220,77 @@ export const ChartProfit = () => {
               ]}
             />
           </VictoryGroup>
+          <VictoryLine
+            interpolation="natural"
+            style={{data: {stroke: '#88C0FF', strokeWidth: 1.5}}}
+            data={[
+              {x: '01', y: 1},
+              {x: '02', y: 4},
+              {x: '03', y: 3},
+              {x: '04', y: 4},
+              {x: '05', y: 2},
+              {x: '06', y: 3},
+              {x: '07', y: 4},
+              {x: '08', y: 3},
+              {x: '09', y: 2},
+              {x: '10', y: 1},
+            ]}
+          />
+          <VictoryLine
+            interpolation="natural"
+            style={{data: {stroke: '#04D632', strokeWidth: 1.5}}}
+            data={[
+              {x: '01', y: 4},
+              {x: '02', y: 2},
+              {x: '03', y: 1},
+              {x: '04', y: 2},
+              {x: '05', y: 2},
+              {x: '06', y: 1},
+              {x: '07', y: 4},
+              {x: '08', y: 3},
+              {x: '09', y: 1},
+              {x: '10', y: 2},
+            ]}
+          />
+          <VictoryLine
+            interpolation="natural"
+            style={{data: {stroke: '#B777CD', strokeWidth: 1.5}}}
+            data={[
+              {x: '01', y: 2},
+              {x: '02', y: 3},
+              {x: '03', y: 4},
+              {x: '04', y: 1},
+              {x: '05', y: 3},
+              {x: '06', y: 2},
+              {x: '07', y: 1},
+              {x: '08', y: 3},
+              {x: '09', y: 2},
+              {x: '10', y: 4},
+            ]}
+          />
+          <VictoryLine
+            interpolation="natural"
+            style={{
+              data: {
+                stroke: '#03ABC2',
+                strokeWidth: 1.5,
+                strokeDasharray: '5,5',
+                strokeDashoffset: 2,
+              },
+            }}
+            data={[
+              {x: '01', y: 3},
+              {x: '02', y: 4},
+              {x: '03', y: 3},
+              {x: '04', y: 2},
+              {x: '05', y: 1},
+              {x: '06', y: 4},
+              {x: '07', y: 1},
+              {x: '08', y: 2},
+              {x: '09', y: 4},
+              {x: '10', y: 3},
+            ]}
+          />
           <VictoryAxis
             gridComponent={<G />}
             tickValues={hourlyData}
@@ -237,6 +310,7 @@ export const ChartProfit = () => {
             gridComponent={<G />}
             tickValues={[1, 2, 4, 5]}
             dependentAxis
+            offsetY={20}
             style={{
               axis: {
                 stroke: isDarkMode ? '#FFFFFF' : '#405385',
@@ -251,6 +325,11 @@ export const ChartProfit = () => {
           />
         </VictoryChart>
       </ScrollView>
+      <View
+        style={[
+          style.containerChecked,
+          {backgroundColor: isDarkMode ? '#414451' : '#F4F7F9'},
+        ]}></View>
     </View>
   );
 };
@@ -290,9 +369,10 @@ const style = StyleSheet.create({
     fontWeight: 400,
     fontSize: 12,
   },
-  containers: {
-    width: '100%',
-    marginTop: 10,
-    paddingLeft: 17,
+  containerChecked: {
+    width: 345,
+    height: 100,
+    marginVertical: 20,
+    borderRadius: 8,
   },
 });
