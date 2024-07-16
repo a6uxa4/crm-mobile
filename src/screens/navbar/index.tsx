32e842d7af953a36c2d78b41a9a2d43083183978
Abu <104144116+a6uxa4@users.noreply.github.com>
2024-07-16 18:22:01 +0600

    fixed

--- src/screens/navbar/index.tsx
+++ src/screens/navbar/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {
   Text,
   View,
@@ -7,6 +7,8 @@ import {
   LayoutChangeEvent,
   useColorScheme,
 } from 'react-native';
+import {ModalDialog, ModalDialogHandle} from 'react-native-propel-kit';
+import {MiniCalendar} from '../../components/MiniCalendar';
 
 interface FilterType {
   productId: string;
@@ -24,6 +26,9 @@ export const Navbar = ({setFilter, filter}: IProps) => {
   const [activeTab, setActiveTab] = useState(0);
   const [translateX] = useState(new Animated.Value(0));
   const [containerWidth, setContainerWidth] = useState(0);
+  const [period, setPeriod] = useState({startPeriod: null, endPeriod: null});
+
+  const modalDialogRef = useRef<ModalDialogHandle>(null);
 
   const tabs = ['сегодня', 'неделя', 'месяц', 'год', 'период'];
 
@@ -33,6 +38,9 @@ export const Navbar = ({setFilter, filter}: IProps) => {
   };
 
   const handleTabPress = (index: number) => {
+    if (index === 4) {
+      modalDialogRef.current.show();
+    }
     setActiveTab(index);
     setFilter({selectType: String(index)});
     const tabWidth = containerWidth / tabs.length;
@@ -42,53 +50,71 @@ export const Navbar = ({setFilter, filter}: IProps) => {
     }).start();
   };
 
+  const handlePeriodChange = newPeriod => {
+    setPeriod(newPeriod);
+  };
+
   return (
-    <View
-      onLayout={handleLayout}
-      style={{
-        width: '90%',
-        height: 40,
-        backgroundColor: isDarkMode ? '#1A2A3D' : '#DBDDE6',
-        borderRadius: 50,
-        marginTop: 20,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-around',
-        position: 'relative',
-      }}>
-      <Animated.View
-        style={{
-          position: 'absolute',
-          width: `${100 / tabs.length}%`,
-          height: '100%',
-          backgroundColor: isDarkMode ? '#6B7188' : '#FFFFFF',
-          borderRadius: 50,
-          transform: [{translateX}],
+    <>
+      <ModalDialog
+        title="Выберите период"
+        confirmTitle="Применить период"
+        cancelTitle="Отмена"
+        ref={modalDialogRef}
+        onCancel={() => {
+          modalDialogRef.current.hide();
+          handleTabPress(0);
         }}
-      />
-      {tabs.map((tab, index) => (
-        <TouchableOpacity
-          key={index}
-          onPress={() => handleTabPress(index)}
+        onConfirm={() => {}}>
+        <MiniCalendar value={period} onChange={handlePeriodChange} />
+      </ModalDialog>
+      <View
+        onLayout={handleLayout}
+        style={{
+          width: '90%',
+          height: 40,
+          backgroundColor: isDarkMode ? '#1A2A3D' : '#DBDDE6',
+          borderRadius: 50,
+          marginTop: 20,
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-around',
+          position: 'relative',
+        }}>
+        <Animated.View
           style={{
+            position: 'absolute',
+            width: `${100 / tabs.length}%`,
             height: '100%',
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-          <Text
+            backgroundColor: isDarkMode ? '#6B7188' : '#FFFFFF',
+            borderRadius: 50,
+            transform: [{translateX}],
+          }}
+        />
+        {tabs.map((tab, index) => (
+          <TouchableOpacity
+            key={index}
+            onPress={() => handleTabPress(index)}
             style={{
-              fontSize: 15,
-              color: isDarkMode
-                ? activeTab === index
-                  ? '#FFFFFF'
-                  : '#8899AA'
-                : '#3D3F44',
+              height: '100%',
+              flex: 1,
+              alignItems: 'center',
+              justifyContent: 'center',
             }}>
-            {tab}
-          </Text>
-        </TouchableOpacity>
-      ))}
-    </View>
+            <Text
+              style={{
+                fontSize: 15,
+                color: isDarkMode
+                  ? activeTab === index
+                    ? '#FFFFFF'
+                    : '#8899AA'
+                  : '#3D3F44',
+              }}>
+              {tab}
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+    </>
   );
 };
